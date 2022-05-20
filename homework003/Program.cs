a0004bc338ad412124d2aca2dc7de4ddb527953d
sirgak <life_4_ever@mail.ru>
2022-05-20 20:10:52 +0300

    teat
    teat

---
+++ homework003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace homework003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pics = 52;
+            int amountPicsInRow = 3;
+            int picsRow = pics / amountPicsInRow;
+            int picsRemainder = pics % amountPicsInRow ;
+            Console.WriteLine("Количество заполненных рядов " + picsRow);
+            Console.WriteLine("Количество картин сверх меры " + picsRemainder);
+        }
+    }
+}
